fe6b70f31333636b19f3ca8dd961b8aee1b57cbb
zhangrenchao <zhangrenchao@newrank.cn>
2022-04-06 22:46:28 +0800

    提交：赋值运算符

---
+++ test/assignment_operation.py
@@ -0,0 +1,25 @@
+a = 3
+a += 2
+print(a)
+
+a -= 1
+print(a)
+
+a *= 2
+print(a)
+
+# 除法
+a /= 2
+print(a)
+
+# 整除操作
+a //= 3
+print(a)
+
+# 取模操作,求余数
+a %= 2
+print(a)
+
+# 幂等于 求幂操作
+a **= 3
+print(a)
