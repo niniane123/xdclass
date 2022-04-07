1b818159bec2fdae45fa7743c8d3cecdae0c3cd2
zhangrenchao <zhangrenchao@newrank.cn>
2022-04-07 23:36:11 +0800

    提交：for循环

---
+++ basic_learning/for_loop.py
@@ -0,0 +1,11 @@
+# for var in sequence:
+#     do someting
+# else :
+#     do something
+#
+# range()生成一个序列,range() 函数从0开始的，不包括最后的数字
+result = 0
+for i in range(101):
+    result += i
+else:
+    print(result)
