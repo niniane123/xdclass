2d038a86c302ea582fe9deb208f3d226cb15eb3d
zhangrenchao <zhangrenchao@newrank.cn>
2022-04-09 22:51:55 +0800

    提交：多返回值以及返回值的接收

--- function/return_multiple_values.py
+++ function/return_multiple_values.py
@@ -11,9 +11,10 @@ def return_multiple_values1():
 
 
 # 接收多个返回值
-
+# 多参数方式接收
 a, b, c = return_multiple_values1()
 print(a, b, c)
+# 单参数接收，随后迭代遍历
 result = return_multiple_values1()
 for i in result:
     print(i)
