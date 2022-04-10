8d01641458ab5ca2b26df1852447001cabfa5086
zhangrenchao <zhangrenchao@newrank.cn>
2022-04-10 22:38:09 +0800

    提交：map函数

---
+++ functional_programing/map.py
@@ -0,0 +1,20 @@
+# map函数
+# map(fun, *iterables),将fuction作用到iterables上，并且作为一个新的iterablesfa返回
+# iterables可以是一个也可以是多个
+
+def add_one(x):
+    return x + 1
+
+
+# 对list中的每一个元素执行了add_one这个函数执行的操做
+result_list = map(add_one, [1, 2, 3])
+# <map object at 0x00000209743EFF10>
+print(result_list)
+print(list(result_list))
+
+my_function = lambda x, y: x + y
+print(map(my_function, [1, 2, 3], [4, 5, 6]))
+
+print(list(map(my_function, [1, 2, 3], [4, 5, 6])))
+# 当可迭代对象中元素个数不一样的时候，以少的为主
+print(list(map(my_function, [1, 2, 3], [4, 5, 6, 3, 4, 5, 6, 7, 8, 9, 0])))
