aced801d9e6a9cbd09a474b0cebca45729fdf34f
zhangrenchao <zhangrenchao@newrank.cn>
2022-04-15 10:49:58 +0800

    提交：闭包：nolocal关键字、闭包函数只能记住最外层的变量值

--- functional_programing/closure.py
+++ functional_programing/closure.py
@@ -1,6 +1,6 @@
 # 闭包
 # 万物都对象，函数也是，那么我们的函数的返回值能否是一个函数呢
-#
+
 
 def my_power():
     n = 2
@@ -28,3 +28,53 @@ print(power(4))
 # 闭包会将环境打印出来 (<cell at 0x000001971118FFD0: int object at 0x0000019710E00110>,)
 print(power.__closure__)
 
+
+def my_power2():
+    n = 2
+
+    def power(x):
+        nonlocal n
+        n += 1
+        return x ** n
+
+    return power
+
+
+my_power2_func = my_power2()
+# 27 3的3次方
+print(my_power2_func(3))
+# (<cell at 0x00000187EA8DF640: int object at 0x00000187EA150130>,)
+print(my_power2_func.__closure__)
+
+
+def my_power3():
+    n = 2
+    fun_list = []
+
+    for i in range(1, 3):
+        # 注意，我们这里定义的power函数并没有直接传参为i,
+        # 当变量是不确定的时候，闭包函数在执行的时候只会记住最外面的一个值——即我们的环境只能支持记忆最外面的值
+        def power():
+            return i ** n
+
+        fun_list.append(power)
+
+    return fun_list
+
+
+func_list = list(my_power3())
+# 2
+print(len(func_list))
+
+print(func_list[0]())  # 4
+
+# (<cell at 0x000002697168F670: int object at 0x0000026971300110>, <cell at 0x000002697168F790: int object at 0x0000026971300110>)
+print(func_list[0].__closure__)
+print(func_list[0].__closure__[0].cell_contents)  # 2
+print(func_list[0].__closure__[1].cell_contents)  # 2
+
+print(func_list[1]())  # 4
+print(func_list[1].__closure__)
+# 原因：当循环结束之后i的值为2
+print(func_list[1].__closure__[0].cell_contents)  # 2
+print(func_list[1].__closure__[1].cell_contents)  # 2
