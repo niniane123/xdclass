020850cd081bbef211f08580b82b3af7dd311934
zhangrenchao <zhangrenchao@newrank.cn>
2022-04-15 22:26:20 +0800

    提交：异常自定义

--- error_and_exception/exception.py
+++ error_and_exception/exception.py
@@ -12,12 +12,11 @@ import warnings
 #     Issue a warning, or maybe ignore it or raise an exception.
 help(warnings.warn)
 
+# def my_function():
+#     warnings.warn("this is a warning", DeprecationWarning)
 
-def my_function():
-    warnings.warn("this is a warning", DeprecationWarning)
 
-
-my_function()
+# my_function()
 
 # 异常的处理格式
 # try:
@@ -38,9 +37,53 @@ finally:
     # 执行结束
     print("执行结束")
 
+
 # 预定义处理来处理异常
 # 在open()的时候无论发生怎样的错误异常，都会关闭我们的IO链接
-with open("my_file.txt") as file:
-    for line in file:
-        print(line, end="")
+# with open("my_file.txt") as file:
+#     for line in file:
+#         print(line, end="")
+
+
 # FileNotFoundError: [Errno 2] No such file or directory: 'my_file.txt'
+
+
+# 使用自定义异常取自定义异常
+# 类的概念
+class self_define_exception(Exception):
+    def __init__(self, parameter):
+        error = "非法入参{0},分母不能为0".format(parameter)
+        # 对Exception做初始化
+        Exception.__init__(self, error)
+        # 这行代码似乎不写也没有影响
+        self.parameter = parameter
+
+
+def my_function(x):
+    if x == 0:
+        # 抛出自定义异常
+        # : self_define_exception.__init__() missing 1 required positional argument: 'parameter'
+        # 这里的x是传给__init__() 函数做初始化的
+        raise self_define_exception(x)
+    else:
+        print(10 / x)
+
+
+my_function(2)
+
+
+# my_function(0)
+
+
+# 捕获异常之后再次抛出异常
+def my_function():
+    try:
+        print(10 / 0)
+    except ZeroDivisionError:
+        print("除数不能为0~")
+        # 再次抛出捕获的异常
+        raise
+
+
+# 除数不能为0~
+my_function()
