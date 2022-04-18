34dbf40347e57c0977e10acbc8ea33d521a57ea7
zhangrenchao <zhangrenchao@newrank.cn>
2022-04-18 17:34:30 +0800

    提交:String_IO以及Bytes_IO学习

--- IO/Bytes _String_IO.py
+++ IO/Bytes _String_IO.py
@@ -33,9 +33,28 @@ from io import BytesIO
 
 bytes_io = BytesIO()
 # 利用encode()将字符串转化为二进制
-bytes_io.write("hello".encode("utf-8"))
+# <editor-fold desc="bytes_io写入信息到内存中">
+bytes_io.write("hello".encode("utf-8"))  # 这是一个注释
 bytes_io.write("world".encode("utf-8"))
-bytes_io.write("张仁超".encode("utf-8"))
+bytes_io.write("张仁超".encode("utf-8"))  # 这是一个注释
+bytes_io.write("zhangsan")
+# </editor-fold>
 
 # 将二进制转化字符串
 print(str(bytes_io.getvalue(), "utf-8"))
+
+from io import BytesIO
+
+# ctrl+f 查找
+# ctrl+r替换  words表示完全匹配  matchcase表示大小写是否忽略
+# ctrl+alt+l
+# 注释 ctrl +/
+# ctrl+alt+方向键前后定位跳转
+
+# ctrl+w代码的快速选中 从内层向外层依次选中，连续按就是连续选中
+# ctrl+shift++(-)代码的展开和压缩（注释 /import/循环体等折叠起来，方便观察代码的结构）
+# ctrl+-(+)折叠部分代码块
+
+# 复制一行ctrl+d
+# 删除一行ctrl+y
+# 移动一行ctrl+shift+方向键
