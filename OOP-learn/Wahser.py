271af6fb3acf82f08a19b73e1c7d9a603f58620a
zhangrenchao <zhangrenchao@newrank.cn>
2022-09-03 19:21:52 +0800

    提交：oop重新学习

---
+++ OOP-learn/Wahser.py
@@ -0,0 +1,17 @@
+# 需求  洗衣机 功能：洗衣服
+# 1.定义洗衣机类
+class Washer():
+
+    # 实例方法，也叫作对象方法，是通过实例对象来调用的
+    def wash_cloth(self):
+        print("正在洗衣服")
+
+
+# 2.创建对象
+washer = Washer()
+# 3.验证成果
+washer.wash_cloth()
+print(washer)  # 打印对象
+
+
+
