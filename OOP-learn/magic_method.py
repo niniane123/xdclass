7e315f4334545376ac2d14fd27a864f5b75907a1
zhangrenchao <zhangrenchao@newrank.cn>
2022-09-04 10:19:08 +0800

    提交：魔法方法__init__(self,参数1，参数2,...)

--- OOP-learn/magic_method.py
+++ OOP-learn/magic_method.py
@@ -20,3 +20,31 @@ washer = Washer()
 washer.width = 100
 washer.height = 100
 washer.print_info()
+
+
+##########################################################################
+# 一个类可以创建多个对象，如何对不同的对象设置不同的初始化属性值呢？——传擦书
+class Washer2():
+    # 除了self参数还添加了height、width的形参，这样子的构造方法（也叫初始化方法）更加有利于对象的初始化
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    # 好像不能像Java那样有多个构造方法
+    # def __init__(self):
+    #     self.height = 0
+    #     self.width = 100
+
+    def print_info(self):
+        print(f"Washer2的高度是{self.height}")
+        print(f"Washer2的高度是{self.width}")
+
+
+# 多个__init()__方法的初试化对象的时候按照
+# washer_2 = Washer2()
+# washer_2.print_info()
+
+washer_3 = Washer2(1000, 2000)
+washer_3.print_info()
+washer_4 = Washer2(700, 800)
+washer_4.print_info()
